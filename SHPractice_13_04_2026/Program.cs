402f7eea0bc6a19627358292c9197ec08814c07b
MikhailDoretsky <pandevichbezpisi@outlook.com>
2026-04-13 20:02:48 +0500

    h

--- SHPractice_13_04_2026/Program.cs
+++ SHPractice_13_04_2026/Program.cs
@@ -11,20 +11,20 @@ namespace CSharp_Practice_13_04_2026
     {
         static void Task1()
         {
-            List<int> indexes = new List<int>();
+            int countindex = 0;
             int[] arr = new int[10] { 1, 2, 3, 4, 0, 5, 1, 9, 0, 0 };
 
             foreach (int i in arr)
             {
                 if (i == 0)
                 {
-                    indexes.Add(i);
+                    countindex += 1;
 
                 }
 
             }
 
-            int[] newarr = new int[arr.Length - indexes.Count];
+            int[] newarr = new int[arr.Length];
 
 
             for (int i = 0, j = 0; i < arr.Length;i++)
@@ -37,10 +37,18 @@ namespace CSharp_Practice_13_04_2026
                 
 
             }
-            for (int i = 0; i < newarr.Length; i++)
+            for(int i = 1; i < countindex + 1;i++)
             {
-                Console.WriteLine(newarr[i]);
+                newarr[newarr.Length - i] = -1;
             }
+
+            arr = newarr;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+
             
 
         }
