eff78c48f64e314a0938e7112e4e5bfa1ede63d3
student <yra.shubin@gmail.com>
2026-04-13 14:59:28 +0500

    Test commit 13_04

--- CSharp_Practice/Program.cs
+++ CSharp_Practice/Program.cs
@@ -10,6 +10,36 @@ namespace CSharp_Practice_13_04_2026
     {
         static void Main(string[] args)
         {
+            Task1();
+
+
+        }
+
+
+        static void Task1()
+        {
+            int[] arr = new int[10] { 0, 0, 1, 2, 3, 4, 5, 0, 0, 0 };
+            
+            foreach (int i in arr)
+            {
+                if (i == 0)
+                {
+                    int indexToRemove = i;
+                    int[] newarr = new int[arr.Length - 1];
+                    foreach (int j in newarr)
+                    {
+                        if (j != i)
+                        {
+                            newarr[j] = arr[i];
+                            
+                        }
+                        arr = newarr;
+                        
+                    }
+                    Console.WriteLine(newarr);
+                }
+
+            }
 
         }
     }
