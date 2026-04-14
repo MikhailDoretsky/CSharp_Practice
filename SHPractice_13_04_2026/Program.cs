118cead11c8049187903d965aff0f34451f33063
MikhailDoretsky <pandevichbezpisi@outlook.com>
2026-04-14 22:35:56 +0500

    h

--- SHPractice_13_04_2026/Program.cs
+++ SHPractice_13_04_2026/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading;
@@ -183,30 +185,234 @@ namespace CSharp_Practice_13_04_2026
 
         }
 
-        static void Task5()
+        static void Task1_Module_2()
         {
+            var rand = new Random();
+
             int[] A = new int[5];
-            int[,] = new int[4, 3];
+            int[,] B = new int[4, 3];
+
+            int line = B.GetLength(0);
+            int Column = B.GetLength(1);
+
+            int GlobalCount = 0;
+            int GlobalProduct = 1;
+            int Honest = 0;
+            int NotEven = 0;
+
+
+            for (int i =0, j = 0; i < line;)
+            {
+                B[i,j] = rand.Next(-25, 25);
+                GlobalCount += B[i, j];
+                GlobalProduct = GlobalProduct * B[i, j];
+
+               
+
+                j++;
+                if (j == Column)
+                {
+                    i++;
+                    j = 0;
+                }
+            }
+            int maxT = B[0, 0];
+            int minT = B[0, 0];
+
+            for (int i = 0, j = 0; i < line;)
+            {
+                Console.Write(B[i, j] + " ");
+
+                if (B[i, j] % 2 != 0)
+                {
+                    NotEven += B[i, j];
+                }
+                if (B[i, j] > maxT)
+                {
+                    maxT = B[i, j];
+                }
+                if (B[i, j] < minT)
+                {
+                    minT = B[i, j];
+                }
+
+                j++;
+                if (j == Column)
+                {
+                    i++;
+                    j = 0;
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine("maximum is " + maxT);
+            Console.WriteLine("minimal is " + minT + "\t");
+            Console.WriteLine("Not even numbers amount is " + NotEven);
+
 
             for (int i = 0; i < A.Length; i++)
             {
                 Console.Write($"\rВведите числое под индексом {i}: ");
-                int input = Convert.ToInt32(Console.Read());
+                int input = Convert.ToInt32(Console.ReadLine());
 
                 A[i] = input;
+                GlobalCount += input;
+                GlobalProduct = GlobalProduct * input;
+                if (A[i] % 2 == 0)
+                {
+                    Honest += A[i];
+                }
+                
             }
-            for (int i = 0; i < A.Length;)
+            int maxD = A[0];
+            int minD = A[0];
+            for (int i = 0; i < A.Length; i++)
             {
-                Console.Write(A[i]);
-                i++;
+               
+                Console.Write(A[i] + ", ");
+                if (A[i] > maxD)
+                {
+                    maxD = A[i];
+                }
+                if (A[i] < minD)
+                {
+                    minD = A[i];
+                }
+
+
 
             }
+            Console.WriteLine("\n" + "maximum is " + maxD);
+            Console.WriteLine("minimal is " + minD);
+            Console.WriteLine("Honest numbers amount is " + Honest + "\n\t");
+
+            if (maxT == maxD)
+            {
+
+                Console.WriteLine("General maximum is " + maxT);
+            }
+            else
+            {
+                Console.WriteLine("General maximum isn't found");
+            }
+
+            if (minT == minD)
+            {
+
+        
+                Console.WriteLine("General maximum is " + minT);
+            }
+            else
+            {
+                Console.WriteLine("General minimal isn't found");
+            }
+            Console.WriteLine("Tatal amount is " + GlobalCount);
+            Console.WriteLine("Tatal product is " + GlobalProduct);
+
+
         }
-       
+
+        static void Task2_Module_2()
+        {
+            int[,] Arr = new int[5, 5];
+
+            var rand = new Random();
+            int Sum = 0;
+
+            int Line = Arr.GetLength(0);
+            int Column = Arr.GetLength(1);
+
+
+            for (int i = 0, j = 0; i < Line;)
+            {
+                Arr[i, j] = rand.Next(-100, 100);
+                Console.Write(Arr[i, j] + " ");
+                j++;
+
+                if (j == Column)
+                {
+                    i++;
+                    j = 0;
+                    Console.WriteLine();
+                }
+            }
+            int Min = Arr[0, 0];
+            int Max = Arr[0, 0];
+
+            for (int i = 0, j = 0; i < Line;)
+            {
+
+                if (Arr[i, j] > Max)
+                {
+                    Max = Arr[i, j];
+                }
+                if (Arr[i, j] < Min)
+                {
+                    Min = Arr[i, j];
+                }
+                j++;
+
+                if (j == Column)
+                {
+                    i++;
+                    j = 0;
+
+                }
+            }
+            int PosMax = Arr[0, 0];
+            int PosMin = Arr[0, 0];
+
+
+
+
+
+
+            for (int i = 0, j = 0; i < Line;)
+            {
+
+                if (Arr[i, j] == Max)
+                {
+                    PosMax = Arr[i, j];
+                }
+                if (Arr[i, j] == Min)
+                {
+                    PosMin = Arr[i, j];
+
+                }
+
+
+                j++;
+
+                if (j == Column)
+                {
+                    i++;
+                    j = 0;
+
+                }
+
+            }
+            for (int d = 0, b = 0; d < Line;)
+            {
+                b++;
+                if (b == Column)
+                {
+                    d++;
+                    b = 0;
+                }
+                if (Arr[d, b] == PosMax)
+                {
+                    while(Arr[d, b] < PosMin)
+                    {
+                        Sum += Arr[d, b];
+                    }
+                }
+            }
+
+        }
         static void Main(string[] args)
         {
-            Task5();
-            
+            Task2_Module_2();
+
+
             Console.Read();
 
         }
