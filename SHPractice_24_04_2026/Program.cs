5a5f7cac9a80cd86cb3a6d2b8d49c6b6c622ab7d
kirillnelemon <kiragataulin2@gmail.com>
2026-04-15 16:14:06 +0500

    H

---
+++ SHPractice_24_04_2026/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SHPractice_24_04_2026
+{
+    internal class Program
+    {
+        
+        static void Task1()
+        {
+            int[] A = new int[10];
+            
+            var rand = new Random();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = rand.Next(100);
+                Console.WriteLine(A[i]);
+            }
+
+            Console.Write("Введите значние: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            int Sum = 0;
+            int NEven = 0;
+            int He = 0;
+
+            for (int i = 0, j = 0; i < A.Length;i++)
+            {
+                if (A[i] % 2 == 0)
+                {
+                    He += 1;
+                    
+                }
+                if (A[i] % 2 != 0)
+                {
+                    NEven += 1;
+                   
+                }
+                if (A[i] != input)
+                {
+                    Sum += 1;
+                    
+                }
+
+
+            }
+            Console.WriteLine("Чётных: " + He);
+            Console.WriteLine("Не чётных: " + NEven);
+            Console.WriteLine("Различных с вашим: " + Sum);
+        }
+        static void Task1()
+
+
+
+        static void Main(string[] args)
+        {
+            Task1();
+        }
+    }
+}
