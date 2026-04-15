91166f30c4948838a807635eaafecf716a65da26
MikhailDoretsky <pandevichbezpisi@outlook.com>
2026-04-15 22:13:16 +0500

    h

--- SHPractice_24_04_2026/Program.cs
+++ SHPractice_24_04_2026/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 
+
 namespace SHPractice_24_04_2026
 {
     internal class Program
     {
-        
+
         static void Task1()
         {
             int[] A = new int[10];
-            
+
             var rand = new Random();
 
             for (int i = 0; i < A.Length; i++)
@@ -24,22 +25,22 @@ namespace SHPractice_24_04_2026
             int NEven = 0;
             int He = 0;
 
-            for (int i = 0, j = 0; i < A.Length;i++)
+            for (int i = 0, j = 0; i < A.Length; i++)
             {
                 if (A[i] % 2 == 0)
                 {
                     He += 1;
-                    
+
                 }
                 if (A[i] % 2 != 0)
                 {
                     NEven += 1;
-                   
+
                 }
                 if (A[i] != input)
                 {
                     Sum += 1;
-                    
+
                 }
 
 
@@ -48,13 +49,15 @@ namespace SHPractice_24_04_2026
             Console.WriteLine("Не чётных: " + NEven);
             Console.WriteLine("Различных с вашим: " + Sum);
         }
-        static void Task1()
+        static void Task2()
+        {
+            int[] Arr = new int[10];
 
-
-
+            var rand = new Random(0);
+        }
         static void Main(string[] args)
         {
-            Task1();
+            Task2();
         }
     }
 }
